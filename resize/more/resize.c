b01e989563d8b0de44fa33a0ff3b78463f23c427
Swapneneel <swapneneel.manipal@gmail.com>
2018-06-13 10:40:53 +0000

    resize thr bmp

---
+++ resize/more/resize.c
@@ -0,0 +1,20 @@
+// Resizing the the bmp with float scale
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmp.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, "Useage: resize scale infile outfile.\n");
+        return 1;
+    }
+    else
+    {
+        float
+    }
+    return 0;
+}
