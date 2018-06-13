03dcb0f968805feb9881e6cc7db0ddf1f595da73
Swapneneel <swapneneel.manipal@gmail.com>
2018-06-13 07:11:20 +0000

    removing header.h

--- recover/header.h
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-typedef uint8_t BYTE;  // it was uint not unit == > uint stands for unsigned integer
-
-typedef struct {
-    BYTE byte_each;
-} __attribute__ ((__packed__))
-JPEG;
--- recover/recover.c
+++ recover/recover.c
@@ -4,8 +4,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#include "header.h"
+// the defiition of struct
+#include <stdint.h>
 
+typedef uint8_t BYTE;  // it was uint not unit == > uint stands for unsigned integer
+
+typedef struct {
+    BYTE byte_each;
+} __attribute__ ((__packed__))
+JPEG;
+
+// definition of struct ended
 
 int main(int argc, char *argv[])
 {
@@ -16,28 +25,32 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // remember filenames
-    char *infile = argv[1];
 
     // open input file
-    FILE *inptr = fopen(infile, "r");
+    FILE *inptr = fopen(argv[1], "r");
     if (inptr == NULL)
     {
-        fprintf(stderr, "Counld not open the %s.raw file.\n", infile);
+        fprintf(stderr, "Counld not open the %s.raw file.\n", argv[1]);
         return 2;
     }  // upto ok
 
-    JPEG *one_box = malloc(512);  // one box with size 512 bytes
+    JPEG *one_box = NULL;  // one box with size 512 bytes
+    /*if (one_box == NULL)  // show error if asked memory not available
+    {
+        fprintf(stderr, "The memory required is not available.\n");
+        printf("The memory required is not available.\n");
+        return 3;
+    } */
 
-    int found = 0, found_prev;
+    int found = 0;
 
     char *outfile = NULL;  // file name string
     FILE *outptr;  // outfile pointer name
 
-    while (fread(one_box, 512, 1, inptr) == 1)
+    while (fread(one_box, sizeof(JPEG), 512, inptr) == 512)
     {
-      if (*(one_box + 0) == 0xff && *(one_box + 1) == 0xd8 && *(one_box + 2) == 0xff &&
-             (*(one_box + 3) & 0xf0) == 0xe0)
+      if (one_box[0].byte_each == 0xff && one_box[1].byte_each == 0xd8 &&
+               one_box[2].byte_each == 0xff && (one_box[3].byte_each & 0xf0) == 0xe0)
       {
           found++;
 
@@ -48,7 +61,7 @@ int main(int argc, char *argv[])
           }
 
           // file name & file opening for reading
-          sprintf(outfile, "%03i.jpg", found);
+          sprintf(outfile, "%03i.jpg", (found - 1));
           outptr = fopen(outfile, "w");  // no need to put the * sign, only write the variable name
           if (outptr == NULL)
           {
@@ -64,9 +77,13 @@ int main(int argc, char *argv[])
       }
 
       // free the alocated memory
-      free(one_box);
+      // one_box = calloc(1, 512);
     }
 
+    // free one_box
+    //free(one_box);
+
+    fclose(inptr);
 
     return 0;  // main on success
 }
