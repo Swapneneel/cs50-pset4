68700164004aec6e022eb993bbad0bab0329c95e
Swapneneel <swapneneel.manipal@gmail.com>
2018-06-14 17:30:21 +0000

    recover.c finally done, with a little help from internet

--- recover/recover.c
+++ recover/recover.c
@@ -9,9 +9,10 @@
 
 typedef uint8_t BYTE;  // it was uint not unit == > uint stands for unsigned integer
 
-typedef struct {
+typedef struct
+{
     BYTE byte_each;
-} __attribute__ ((__packed__))
+} __attribute__((__packed__))
 JPEG;
 
 // definition of struct ended
@@ -27,8 +28,8 @@ int main(int argc, char *argv[])
 
 
     // open input file
-    FILE *inptr = fopen(argv[1], "r");
-    if (inptr == NULL)
+    FILE *card = fopen(argv[1], "r");
+    if (card == NULL)
     {
         fprintf(stderr, "Counld not open the %s.raw file.\n", argv[1]);
         return 2;
@@ -38,65 +39,60 @@ int main(int argc, char *argv[])
 
     int found = 0;
 
-    char *outfile = NULL;  // file name string
-    FILE *outptr_opened = NULL;  // outfile pointer name
+    char outfile[8];  // file name char array
+    FILE *img = NULL;  // outfile pointer name
 
-    fseek(inptr, 0L, SEEK_END);
-    long int n = ftell(inptr) / 512;
-    // printf("n = %li\n", n);
-
-    fseek(inptr, 0L, SEEK_SET);
-
-    for (long int i = 0; i < n; i++)
+    JPEG *one_box = malloc(sizeof(JPEG) * 512);  // one box with size 512 bytes
+    if (one_box == NULL)  // show error if asked memory not available
     {
-        JPEG *one_box = malloc(sizeof(JPEG) * 512);  // one box with size 512 bytes
-        if (one_box == NULL)  // show error if asked memory not available
-        {
-            fprintf(stderr, "The memory required is not available.\n");
-            printf("The memory required is not available.\n");
-            return 3;
-        }
+        fprintf(stderr, "The memory required is not available.\n");
+        printf("The memory required is not available.\n");
+        return 3;
+    }
 
-        fread(one_box, sizeof(JPEG), 512, inptr);
-
+    // till the end of the file card
+    while (fread(one_box, sizeof(one_box), 1, card) > 0)  // fread on successfull transection returns no of files read
+    {
+        // this will check all the possible JPEG header options
         if (one_box[0].byte_each == 0xff && one_box[1].byte_each == 0xd8 &&
-               one_box[2].byte_each == 0xff && (one_box[3].byte_each & 0xf0) == 0xe0)
+            one_box[2].byte_each == 0xff && (one_box[3].byte_each & 0xf0) == 0xe0)
         {
-              found++;
+            // it will count up when a new JPEG is found
+            found++;
 
             // close the file previously opened
             if (found > 1)
             {
-                fclose(outptr_opened);
+                fclose(img);
             }
 
             // file name & file opening for reading
             sprintf(outfile, "%03i.jpg", (found - 1));
-            FILE *outptr = fopen(outfile, "w");  // no need to put the * sign, only write the variable name
-            if (outptr == NULL)
+            img = fopen(outfile, "a");  // no need to put the * sign, only write the variable name
+            if (img == NULL)
             {
                 // error message for not opening the outfile
                 fprintf(stderr, "Could not create %s.\n", outfile);
-                return 3;
+                return 4;
             }
-            *outptr = *outptr_opened;
-        }
+            // write 512 byte at once
+            fwrite(one_box, sizeof(one_box), 1, img);
 
-        if (found > 0)  // wtite only when after found the 1st one
+        }
+        else
         {
-            fwrite(one_box, sizeof(JPEG), 512, outptr_opened);  // 512 byte each time
+            if (found > 0)  // it will write if a file is already opened
+            {
+                fwrite(one_box, sizeof(one_box), 1, img);  // 512 byte each time
+            }
         }
 
-        // fseek(inptr, 512, SEEK_CUR);
-
-        // free the alocated memory
-        free(one_box);
     }
+    // free img
+    free(img);
 
-    // free one_box
-    //free(one_box);
-
-    fclose(inptr);
+    // close the card
+    fclose(card);
 
     return 0;  // main on success
 }
